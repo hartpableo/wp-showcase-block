e9e9542ecab555e5d2a94c2fbb5bc971678152b4
Hart <pableoh@gmail.com>
2023-08-05 06:48:11 +0800

    init

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '57b2a0dc8a34f9d60f73');
---
+++ build/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '3d7cb40fe7e7fa2859f8');
